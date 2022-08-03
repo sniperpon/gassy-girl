9b40966d85128dfb14f85ee688b343c8d9a03b25
Ponicki <11429394+sniperpon@users.noreply.github.com>
2022-08-03 06:51:26 -0500

    Javascript interop tinkering (#5)
    * Put some helpful links into the readme
    
    * Got CRUD for mileage one hundred percent working!

--- GassyGirl/Client/Shared/Facades/IDatabaseFacade.cs
+++
@@ -1,9 +0,0 @@
-public interface IDatabaseFacade
-{
-    List<Dictionary<string, object>> GetMileages();
-    List<Dictionary<string, object>> GetCars();
-    bool SaveMileage(MileageRecord mileageRecord);
-    bool SaveCar(Car car);
-    bool DeleteMileage(MileageRecord mileageRecord);
-    bool DeleteCar(Car car);
-}
